d10bc26164db856aa945ac96ef995a1325071c5f
Thomas Wilburn <news@thomaswilburn.net>
2015-11-09 14:56:08 -0800

    Brightcove works (or as close as it's going to get)

---
+++ src/js/brightcove.js
@@ -0,0 +1,19 @@
+var callbacks = [];
+var player;
+
+var src="//players.brightcove.net/1509317113/default_default/index.min.js";
+var script = document.createElement("script");
+script.src = src;
+script.onload = function() {
+  player = videojs("player").ready(function() {
+    callbacks.forEach(fn => fn(player));
+  });
+};
+document.head.appendChild(script);
+
+module.exports = function(fn) {
+  if (player) {
+    return fn(player);
+  }
+  callbacks.push(fn);
+};
--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,58 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+var ready = require("./brightcove");
+var dot = require("./lib/dot");
+var playlistTemplate = dot.compile(require("./_playlist.html"));
+var playlistContainer = document.querySelector(".playlist-container");
+
+var ids = [4594952965001, 4558264932001, 4567072591001, 4556052811001, 4537656765001, 4518683229001];
+var playlistID = 4539370305001;
+
+var log = console.log.bind(console);
+
+var closest = function(element, className) {
+  while (element && !element.classList.contains(className)) element = element.parentElement;
+  return element;
+};
+
+ready(function(player) {
+  window.player = player;
+
+  player.catalog.getPlaylist(playlistID, function(err, playlist) {
+    playlistContainer.innerHTML = playlistTemplate(playlist);
+    player.catalog.load(playlist);
+
+    var lookup = {};
+    playlist.forEach((v, i) => lookup[v.id] = v);
+
+    playlistContainer.addEventListener("click", function(e) {
+      if (!playlistContainer.classList.contains("enabled")) return;
+      var li = closest(e.target, "playlist-item");
+      var id = li.getAttribute("data-id");
+      var index = player.playlist.indexOf(lookup[id]);
+      player.playlist.currentItem(index);
+      player.play();
+    });
+
+    var update = function(e) {
+      var active = document.querySelector("li.playlist-item.active");
+      if (active) active.classList.remove("active");
+      var playingAd = player.ads.state == "ad-playback";
+      playlistContainer.setAttribute("data-enabled", !playingAd);
+      if (playingAd) return;
+      playlistContainer.classList.add("enabled");
+      if (player.paused()) return;
+      var id = player.mediainfo.id;
+      var li = document.querySelector(`li[data-id="${id}"]`);
+      if (li) {
+        li.classList.add("active");
+      }
+    };
+
+    "play playing blocked adstart adend loadstart loadedmetadata loadeddata".split(" ").forEach(e => player.on(e, update));
+
+  });
+});
+
+
